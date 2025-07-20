30bab672b9c55c88e3995466ebb541da9694be8b
quan <quan93909@gmail.com>
2025-07-20 14:31:32 +0700

    Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,26 +2,32 @@ import { Request, Response }  from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res: Response) => {
+  // Find
   interface Find {
     deleted: boolean,
     status?: string
   }
-
-  // const find = {
-  //    deleted: false,
-  // }
 
   const find: Find = {
      deleted: false,
   }
 
   if(req.query.status) {
-    // find["status"] = req.query.status; // Cách 1
     find.status = req.query.status.toString();
-
   }
 
-  const tasks = await Task.find(find);
+  // End find
+
+  // Sort
+  const sort = {};
+  
+  if(req.query.sortKey && req.query.sortValue) {
+    const sortKey = req.query.sortKey.toLocaleString();
+    sort[sortKey] = req.query.sortValue;
+  }
+  // End Sort
+
+  const tasks = await Task.find(find).sort(sort);
 
   res.json(tasks);
 }
